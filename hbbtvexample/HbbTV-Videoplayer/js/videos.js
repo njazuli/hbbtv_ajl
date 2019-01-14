e6f7d731c277b57c58be21eb2f46a1fd98fee525
Nuri Jazuli <nurijazuli@tonton.com.my>
2019-01-15 01:00:30 +0800

    done for videos tab
    done for videos tab

--- hbbtvexample/HbbTV-Videoplayer/js/videos.js
+++ hbbtvexample/HbbTV-Videoplayer/js/videos.js
@@ -6,35 +6,42 @@
 
 //When the window is load this function is executed
 window.onload = function() {
-
-	(function(){
-
-		var listEl = document.querySelector('.home-grid.products-grid.products-grid--max-4');
-		var btnLeftEl = document.querySelector('#left-btn');
-		var btnRightEl = document.querySelector('#right-btn');
-		var count = 0;
-
-		function slideImages(dir){
-			var totalChildren = listEl.querySelectorAll(".item").length;
-			dir === "left" ? ++count : --count;
-			listEl.style.left = count * 290 + 'px';
-			btnLeftEl.style.display = count < 0 ? "block" : "none";
-			// Here, 4 is the number displayed at any given time
-			btnRightEl.style.display = count > 3 -totalChildren ? "block" : "none";
-		}
-
-		btnLeftEl.addEventListener("click", function(e) {
-		  slideImages("left");
-		});
-		btnRightEl.addEventListener("click", function(e) {
-		  slideImages("right");
-		});
-	})();
-	
-	console.log("in");
 	//localStorage.removeItem("videos");
+
+	//function for carousel
+	var listEl = document.querySelector('.home-grid.products-grid.products-grid--max-4');
+	var btnLeftEl = document.querySelector('#left-btn');
+	var btnRightEl = document.querySelector('#right-btn');
+	var count = 0;
+
+	function slideImages(dir){
+		
+		var totalChildren = listEl.querySelectorAll(".item-container").length;
+		dir === "left" ? ( count < 0 ? ++count : 0 ) : (count <= -(totalChildren-1) ? count : --count);
+		listEl.style.left = count * 284 + 'px';
+		btnLeftEl.style.display = count < 0 ? "block" : "none";
+		// Here, 4 is the number displayed at any given time
+		btnRightEl.style.display = count > 2 -totalChildren ? "block" : "none";
+
+		var currentcount = (count* -1) +1;
+
+		console.log(count * -1);
+		$('.item-container:nth-child(' + currentcount + ')').focus();
+
+
+		
+	}
+
+	btnLeftEl.addEventListener("click", function(e) {
+	  slideImages("left");
+	});
+	btnRightEl.addEventListener("click", function(e) {
+	  slideImages("right");
+	});
+
 	getData();
 	//MANAGER SETTINGS
+
 	var appManager = document.getElementById("oipfAppMan").getOwnerApplication(document);
 	appManager.show();
 	appManager.privateData.keyset.setValue(0x1 + 0x2 + 0x4 + 0x8 + 0x10 + 0x100);
@@ -58,6 +65,7 @@ window.onload = function() {
 				console.log("YELLOW - Stop Video");
 			break;
 			case VK_UP:
+				// goUp();
 				console.log("UP - Move up focus");
 			break;
 			case VK_DOWN:
@@ -65,14 +73,16 @@ window.onload = function() {
 			break;
 			case VK_LEFT:
 				moveLeft();
+				slideImages("left");
 				console.log("LEFT - Move left focus");
 			break;
 			case VK_RIGHT:
+				slideImages("right");
 				moveRight();
 				console.log("RIGHT - Move right focus");
 			break;
 			case VK_ENTER:
-				// getData();
+				gotolink();
 				console.log("ENTER - Ok pressed");
 			break;
 			case VK_0:
@@ -86,27 +96,39 @@ window.onload = function() {
 };
 
 
-
 function goHome() {
 	window.location.href = 'index.html';
 }
 
+function goUp(){
+	var top_element = document.getElementById("player");
+	top_element.focus();
+}
+
+function goDown(){
+	var bottom_element = document.getElementById("carousel_list");
+	bottom_element.focus();
+}
+
 function moveLeft() {
 	var index = $(":focus").index() + 1;
+	console.log('index' + index);
+
 	$('#menulist_div .menulist_item:nth-child(' + (index - 1) + ')').focus();
 }
 
 function moveRight() {
 	var index = $(":focus").index() + 1;
 	$('#menulist_div .menulist_item:nth-child(' + (index + 1) + ')').focus();
+	console.log('index' + index);
 }
 
 function gotolink(){
-
+	// playVideo(video_id);
 	var index = $(":focus").index() + 1;
-	var href = $('#menulist_div .menulist_item:nth-child(' + index + ') a').attr('href');
+	var href = $('.item-container:nth-child(' + index + ') a').attr('href');
 
-	window.location.href = href;
+	playVideo(href);
 	console.log('okay or red button is clicked' + href);
 }
 
@@ -164,28 +186,35 @@ function loadCarousel(data){
 		title =  construct_data[i].title;
 		video_id =  construct_data[i].video_id;
 
+		//play video for first id first
+		if( i === 0 ){
+			playVideo(video_id);
+		}
+
 		//create first div
 		var videos_div = document.createElement("div");
 		videos_div.className = "item-container";
+		videos_div.setAttribute('tabindex', '0');
+		
 
 		//create a tag
 		var videos_a_tag = document.createElement("a");
-		videos_a_tag.setAttribute('href', 'video_id');
+		videos_a_tag.setAttribute('href', video_id);
 
 
 		//create second div
 		var item_div = document.createElement("div");
-		item_div.className = "item full_width";
+		item_div.className = "item scale-16_9";
 
 		//create image tag
 		var item_img = document.createElement("img");
 		item_img.src = img_src;
 		item_img.setAttribute("alt","ajl33");
-		item_img.className = "sixteen-nine";
+		// item_img.className = "sixteen-nine";
 
 		//create div for p
 		var p_div = document.createElement("div");
-		p_div.className = "item full_width";
+		p_div.className = "introsub full_width";
 
 		//create p tag for title
 		var item_title = document.createElement("p");
@@ -194,12 +223,14 @@ function loadCarousel(data){
 
 
 		//append img in second div
+		videos_a_tag.appendChild(item_div);
+		videos_a_tag.appendChild(p_div);
 		item_div.appendChild(item_img);
 		p_div.appendChild(item_title);
 
 		//append second div and p inside a
-		videos_div.appendChild(item_div);
-		videos_div.appendChild(p_div);
+		videos_div.appendChild(videos_a_tag);
+		// videos_div.appendChild(p_div);
 		// videos_div.appendChild(item_title);
 
 		//append a inside first div
@@ -209,7 +240,17 @@ function loadCarousel(data){
 		placeholder.appendChild(videos_div);
 	}
 
-	
+	$('.item-container:nth-child(1)').focus();
 }
+
+
+function playVideo(id) {
+	var playerid = document.getElementById('player');
+	var static_url = "https://www.youtube.com/embed/"
+	
+	playerid.src = static_url + id + '?autoplay=1&controls=0';
+}
+
+
 
 
